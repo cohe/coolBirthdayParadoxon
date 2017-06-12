fcba0fc35489a7466ba223f32c661210f94e2e03
coni <coni@Coni-Laptop-Samsung>
2017-06-12 22:29:12 +0200

    Rename main function

---
+++ src/coolgeburtstagsparadoxon/coolGeburtstagsParadoxon.java
@@ -0,0 +1,12 @@
+package coolgeburtstagsparadoxon;
+
+public class coolGeburtstagsParadoxon {
+
+    public static void main(String[] args) {
+        
+        Result result = new Result();
+        
+        result.printResults();
+    }
+    
+}
